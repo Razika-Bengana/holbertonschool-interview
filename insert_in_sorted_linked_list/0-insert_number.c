abfa2fc8aeffb837cff9ee0c83e2897cae5e6a2b
Razika-Bengana <5003@holbertonstudents.com>
2023-03-30 00:22:42 +0200

    insert a number into a sorted singly linked list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to the head of the list
+ * @number: number to be inserted
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *current;
+
+/* Allocate memory for the new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+/* Initialize the new node */
+	new_node->n = number;
+	new_node->next = NULL;
+
+/* If the list is empty, insert the new node as the head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+/* If the number is smaller than the head, insert the new node as the head */
+	if (number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+/* Traverse the list to find the correct position to insert the new node */
+	current = *head;
+	while (current->next != NULL && current->next->n <= number)
+	{
+		current = current->next;
+	}
+
+/* Insert the new node */
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+}
