c3502103cea78502a1a0b3c31ad8152b1ff6f5a9
Razika-Bengana <5003@holbertonstudents.com>
2023-04-21 03:32:59 +0200

    header file

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,8 +1,10 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-
-void topple(int grid[3][3]);
+void print_sandpile(int grid[3][3]);
+int check_is_stable(int grid1[3][3], int grid2[3][3]);
+void sum_grids(int grid1[3][3], int grid2[3][3]);
+void toppling_round(int grid1[3][3], int grid2[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif /* SANDPILES_H */
