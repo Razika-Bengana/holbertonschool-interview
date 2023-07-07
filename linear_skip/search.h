84e01dc371ae86f3dafd7228e1a0a246fa02ba60
Razika-Bengana <5003@holbertonstudents.com>
2023-07-07 04:36:16 +0200

    correct betty style

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -18,12 +18,13 @@
  * Description: singly linked list node structure with an express lane
  * for Holberton project
  */
+
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *create_skiplist(int *array, size_t size);
