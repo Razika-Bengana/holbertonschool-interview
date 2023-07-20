9c1f05346130509ffa16463cfa33ea8d5c3d4073
Razika-Bengana <5003@holbertonstudents.com>
2023-07-20 18:33:26 +0200

    0. Rain

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Calculates water between walls """
 
-
 def rain(walls):
     """ Calculate the amount of water trapped between walls """
 
@@ -12,38 +11,17 @@ def rain(walls):
     # Initialize the variable to keep track of the total trapped water units
     total_water_units = 0
 
-    # Initialize the left wall index
-    left_wall_index = 0
-
     # Get the number of walls
     walls_len = len(walls)
 
     # Iterate through the list of walls
-    for current_wall_index in range(0, walls_len - 2):
+    for current_wall_index in range(1, walls_len - 1):
+        left_max = max(walls[:current_wall_index])
+        right_max = max(walls[current_wall_index + 1:])
 
-        # Check if the current wall is higher than 0 and not already processed
-        if walls[current_wall_index] > 0 and current_wall_index >=\
-           left_wall_index:
-
-            """
-            Look for the next wall that is higher or equal to the current wall
-            """
-            for next_wall_index in range(current_wall_index + 2, walls_len):
-
-                if walls[next_wall_index] >= walls[current_wall_index]:
-                    """
-                    Calculate the trapped water between the current wall
-                    and the next wall
-                    """
-                    water_level = walls[current_wall_index]
-
-                    for wall_between_index in range(current_wall_index + 1,
-                                                    next_wall_index):
-                        total_water_units += water_level -\
-                            walls[wall_between_index]
-                    # Update the left wall index to the next wall
-                    left_wall_index = next_wall_index
-
-                    break
+        # Calculate the trapped water between the current wall and the lower of the two boundary walls
+        water_level = min(left_max, right_max)
+        if water_level > walls[current_wall_index]:
+            total_water_units += water_level - walls[current_wall_index]
 
     return total_water_units
