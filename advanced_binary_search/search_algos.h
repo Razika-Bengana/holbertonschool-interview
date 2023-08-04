b1d12f2bc2af4fd617409bf7f57c075b22dc46ca
Razika-Bengana <5003@holbertonstudents.com>
2023-08-04 13:26:00 +0200

    Header file

---
+++ advanced_binary_search/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+
+int recursive_advanced_binary(int *array, int low, int high, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALGOS_H */
