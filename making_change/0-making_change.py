b240316f72f49b1cfc226e3845eb570c6917cff1
Razika-Bengana <5003@holbertonstudents.com>
2023-09-22 12:20:12 +0200

    0. Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -9,27 +9,21 @@ def makeChange(coins, total):
     Given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
     """
-    if type(total) is not int or type(coins) is not list or not all(
-            [type(coin) is int for coin in coins]):
-        print("Invalid arguments")
-        return 0
-
     if total <= 0:
         return 0
-
-    coins.sort(reverse=True)
-
-    total_coins = 0
-    remainder = total
-    for denomination in coins:
-        if denomination <= remainder:
-            num_of_coins = remainder // denomination
-            total_coins += num_of_coins
-            remainder -= num_of_coins * denomination
-        if remainder == 0:
-            break
-
-    if total_coins == 0 or remainder != 0:
+    if len(coins) <= 0:
         return -1
 
-    return total_coins
+    sc = sorted(coins)
+    sc = sc[::-1]
+
+    num = 0
+    running = total
+    for c in sc:
+        while (running - c >= 0):
+            running = running - c
+            num = num + 1
+
+    if running != 0 and running - sc[-1] < 0:
+        return -1
+    return num
