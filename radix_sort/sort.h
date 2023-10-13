a6a79bd2481b9a425a29cbfe773df28029a56cc9
Razika-Bengana <5003@holbertonstudents.com>
2023-10-13 23:23:51 +0200

    Header file

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -4,12 +4,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <limits.h>
 
 
 void print_array(const int *array, size_t size);
-void counting_sort(int *array, size_t size, int digit);
 void radix_sort(int *array, size_t size);
+int sort_by_current_digit(int *array, ssize_t size, int *tmp, long exp);
 
 
 #endif /* SORT_H */
