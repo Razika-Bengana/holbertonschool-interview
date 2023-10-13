96685a40151f2381947dbeb89c085eb51029c222
Razika-Bengana <5003@holbertonstudents.com>
2023-10-13 23:24:07 +0200

    0. Radix sort

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -1,88 +1,64 @@
 #include "sort.h"
 
-
 /**
- * counting_sort - Program that performs counting sort on an array
- * based on a specific digit
+ * sort_by_current_digit - program that sorts the array based on
+ * the current significant digit
  *
  * @array: the array to sort
  * @size: the size of the array
- * @digit: the place value to sort the array on (1s, 10s, 100s, etc.)
+ * @tmp: temporary array used for sorting
+ * @exp: the current digit to sort by, represented as a power of 10
  *
- * Return: nothing (void)
+ * Return: always returns 0, used for compatibility with other
+ * sorting functions
  */
-
-void counting_sort(int *array, size_t size, int digit)
+int sort_by_current_digit(int *array, ssize_t size, int *tmp, long exp)
 {
-	int *output = malloc(size * sizeof(int));
-	int count[10] = {0};
-	int d;
-	size_t i;
-	int j;
+	ssize_t i;
+	int aux[10] = {0};
 
-	if (output == NULL)
-		return;
-
-/* Count occurrences of each digit at the given place */
 	for (i = 0; i < size; i++)
-	{
-		d = (array[i] / digit) % 10;
-		count[d]++;
-	}
-
-/* Calculate the position of each element */
-	for (j = 1; j < 10; j++)
-	{
-		count[j] += count[j - 1];
-	}
-
-/* Build the output array */
-	for (j = size - 1; j >= 0; j--)
-	{
-		d = (array[j] / digit) % 10;
-		output[count[d] - 1] = array[j];
-		count[d]--;
-	}
-
-/* Copy the output array to the original array */
+		aux[(array[i] / exp) % 10]++, tmp[i] = 0;
+	for (i = 1; i < 10; i++)
+		aux[i] += aux[i - 1];
+	for (i = size - 1; i >= 0; i--)
+		tmp[--aux[(array[i] / exp) % 10]] = array[i];
 	for (i = 0; i < size; i++)
-	{
-		array[i] = output[i];
-	}
-	free(output);
+		array[i] = tmp[i];
+	return (0);
 }
 
 /**
- * radix_sort - Program that performs radix sort on an array
- * of non-negative integers
+ * perform_radix_sort - Program that sorts an array of integers using
+ * Radix sort algorithm
  *
- * @array: the array to sort
- * @size: the size of the array
+ * @array: The array to sort
+ * @size: The size of the array
  *
  * Return: nothing (void)
  */
 
 void radix_sort(int *array, size_t size)
 {
-
-/* Find the maximum element to determine the number of digits */
-	int max;
 	size_t i;
-	int digit;
+	long exp = 1;
+	int *tmp, max = INT_MIN;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-		{
-			max = array[i];
-		}
-	}
+	if (!array || size < 2)
+		return;
 
-/* Perform counting sort for each digit */
-	for (digit = 1; max / digit > 0; digit *= 10)
+	tmp = malloc(sizeof(int) * size);
+	if (!tmp)
+		return;
+
+	for (i = 0; i < size; i++)
+		max = array[i] > max ? array[i] : max;
+
+	while (max / exp > 0)
 	{
-		counting_sort(array, size, digit);
+		sort_by_current_digit(array, size, tmp, exp);
 		print_array(array, size);
+		exp *= 10;
 	}
+	free(tmp);
 }
