7b7198275b55ae7f48f301f040866c138031b61a
RazikaBengana <razikabengana@gmail.com>
2023-11-22 14:43:47 +0100

    Header file

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -22,10 +22,9 @@ typedef struct binary_tree_s
 
 typedef struct binary_tree_s heap_t;
 
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
+
 void binary_tree_print(const binary_tree_t *tree);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
+heap_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif /* _BINARY_TREES_H_ */
