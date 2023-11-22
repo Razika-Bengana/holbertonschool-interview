cdd5fab664e32eedbe714692a6fdac6c02b8c198
RazikaBengana <razikabengana@gmail.com>
2023-11-22 14:46:22 +0100

    Correct Betty style

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -11,14 +11,14 @@
 
 heap_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    heap_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+        heap_t *new_node = malloc(sizeof(binary_tree_t));
+        if (new_node == NULL)
+                return (NULL);
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+        new_node->n = value;
+        new_node->parent = parent;
+        new_node->left = NULL;
+        new_node->right = NULL;
 
-    return (new_node);
+        return (new_node);
 }
